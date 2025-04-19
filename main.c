9d75efc65b0eca21fd4e7f0e61c18d5fa1f22a85
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-19 01:27:30 -0300

    Organizando codigo e tratando entrada de usuario

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define NUMERO_CHUTES 3
 
 int main(void){
   printf("************************************\n");
@@ -8,22 +9,29 @@ int main(void){
   int numeroSecreto = 42;
   int chute;
 
-for (int i = 1; i <=3; i++) {
-  printf("Tentativa %d/3\n", i);
+for (int i = 1; i <=NUMERO_CHUTES; i++) {
+  printf("Tentativa %d/%d\n", i, NUMERO_CHUTES);
   printf("Qual e o seu chute: ");
   scanf("%d", &chute);
+
+  if (chute < 0) {
+    printf("vOCE NAO PODE CHUTAR NUMEROS NEGATIVOS!\n");
+    i--; // decrementa o contador de tentativas
+    continue;
+  }
+
   if (chute == numeroSecreto) {
     printf("Parabens, voce acertou o numero secreto :)\n");
     break;
-  }else {
+  }else if (chute > numeroSecreto) {
     printf("chute foi errado! Tente novamente!\n");
-    if (chute > numeroSecreto) {
-      printf("Dica: o chute foi maior que o numero secreto!\n");
-    }else {
+    printf("Dica: o chute foi maior que o numero secreto!\n");
+  }else {
+      printf("chute foi errado! Tente novamente!\n");
       printf("Dica: O chute foi menor que o numero secreto\n");
     }
 }
-  }
+
   printf("\nFim de jogo!");
 
   return 0;
