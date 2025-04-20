aa10b1824a41a88051595572dae954bdeed9be0d
Ivirton-Filho <ivirtonfilho@gmail.com>
2025-04-19 22:20:33 -0300

    Add dificuldade (difícil)

--- main.c
+++ main.c
@@ -19,31 +19,72 @@ int main(void) {
 
     system("pause");
 
-    int segundos = time(0);
-    srand(segundos);
-    int numeroSecreto = rand() % 50;
-    int chute;
-    float pontos = 1000; //jogador comeca com 1000 pontos
-//laço de repeticao que faz o codigo repetir as tentativas de chance
+      int segundos = time(0);
+      srand(segundos);
+      int numeroSecreto = rand() % 50;
+      int chute;
+      float pontos = 1000; //jogador comeca com 1000 pontos
+  //laço de repeticao que faz o codigo repetir as tentativas de chance
+      for (int i = 1; i <=NUMERO_CHUTES; i++) {
+        printf("Tentativa %d/%d\n", i, NUMERO_CHUTES);
+        printf("Qual e o seu chute: ");
+        fflush(stdin); // limpar buffer (serve apenas para windows)
+        scanf(" %d", &chute);
+  //mensagem caso o jogador coloque um numero negativo
+        if (chute < 0) {
+          printf("vOCE NAO PODE CHUTAR NUMEROS NEGATIVOS!\n");
+          i--; // decrementa o contador de tentativas
+          continue; //pulatodo o restande de codigo e volta para o laço
+        }
+  //mensagem caso o jogador acerte o numero ou que apresenta dicas ao jogador
+        if (chute == numeroSecreto) {
+          printf("Parabens, voce acertou o numero secreto :)\n");
+          break;
+        }else if (chute > numeroSecreto) {
+          printf("chute foi errado! Tente novamente!\n");
+          printf("Dica: o chute foi maior que o numero secreto!\n");
+          pontos = pontos - abs((chute - numeroSecreto) / 2.0); // sistema que subtrai a quantidade de pontos
+        }else {
+          printf("chute foi errado! Tente novamente!\n");
+          printf("Dica: O chute foi menor que o numero secreto\n");
+          pontos = pontos - abs((chute - numeroSecreto) / 2.0); // sistema que subtrai a quantidade de pontos
+        }
+      }
+
+      printf("\nFim de jogo!\n");
+      printf("Pontuacao total:%.1f", pontos);
+      break;
+
+    case 2:
+      printf("Dificuldade escolhida: MEDIO\n");
+
+    system("pause");
+
+      segundos = time(0);
+      srand(segundos);
+      numeroSecreto = rand() % 150;
+      chute;
+      pontos = 1000;//jogador comeca com 1000 pontos
+    //laço de repeticao que faz o codigo repetir as tentativas de chance
     for (int i = 1; i <=NUMERO_CHUTES; i++) {
       printf("Tentativa %d/%d\n", i, NUMERO_CHUTES);
       printf("Qual e o seu chute: ");
       fflush(stdin); // limpar buffer (serve apenas para windows)
       scanf(" %d", &chute);
-//mensagem caso o jogador coloque um numero negativo
+      //mensagem caso o jogador coloque um numero negativo
       if (chute < 0) {
         printf("vOCE NAO PODE CHUTAR NUMEROS NEGATIVOS!\n");
         i--; // decrementa o contador de tentativas
         continue; //pulatodo o restande de codigo e volta para o laço
       }
-//mensagem caso o jogador acerte o numero ou que apresenta dicas ao jogador
+      //mensagem caso o jogador acerte o numero ou que apresenta dicas ao jogador
       if (chute == numeroSecreto) {
         printf("Parabens, voce acertou o numero secreto :)\n");
         break;
       }else if (chute > numeroSecreto) {
         printf("chute foi errado! Tente novamente!\n");
         printf("Dica: o chute foi maior que o numero secreto!\n");
-        pontos = pontos - abs((chute - numeroSecreto) / 2.0); // sistema que subtrai a quantidade de pontos
+        pontos = pontos - abs((chute - numeroSecreto) / 2); // sistema que subtrai a quantidade de pontos
       }else {
         printf("chute foi errado! Tente novamente!\n");
         printf("Dica: O chute foi menor que o numero secreto\n");
@@ -55,40 +96,40 @@ int main(void) {
     printf("Pontuacao total:%.1f", pontos);
     break;
 
-    case 2:
-      printf("Dificuldade escolhida: MEDIO\n");
+    case 3:
+      printf("Dificuldade escolhida: DIFICIL\n");
 
     system("pause");
 
      segundos = time(0);
     srand(segundos);
-     numeroSecreto = rand() % 150;
+     numeroSecreto = rand() % 250;
      chute;
-     pontos = 1000;
-
+     pontos = 1000; //jogador comeca com 1000 pontos
+    //laço de repeticao que faz o codigo repetir as tentativas de chance
     for (int i = 1; i <=NUMERO_CHUTES; i++) {
       printf("Tentativa %d/%d\n", i, NUMERO_CHUTES);
       printf("Qual e o seu chute: ");
       fflush(stdin); // limpar buffer (serve apenas para windows)
       scanf(" %d", &chute);
-
+      //mensagem caso o jogador coloque um numero negativo
       if (chute < 0) {
         printf("vOCE NAO PODE CHUTAR NUMEROS NEGATIVOS!\n");
         i--; // decrementa o contador de tentativas
-        continue;
+        continue; //pulatodo o restande de codigo e volta para o laço
       }
-
+      //mensagem caso o jogador acerte o numero ou que apresenta dicas ao jogador
       if (chute == numeroSecreto) {
         printf("Parabens, voce acertou o numero secreto :)\n");
         break;
       }else if (chute > numeroSecreto) {
         printf("chute foi errado! Tente novamente!\n");
         printf("Dica: o chute foi maior que o numero secreto!\n");
-        pontos = pontos - abs((chute - numeroSecreto) / 2);
+        pontos = pontos - abs((chute - numeroSecreto) / 2.0); // sistema que subtrai a quantidade de pontos
       }else {
         printf("chute foi errado! Tente novamente!\n");
         printf("Dica: O chute foi menor que o numero secreto\n");
-        pontos = pontos - abs((chute - numeroSecreto) / 2.0);
+        pontos = pontos - abs((chute - numeroSecreto) / 2.0); // sistema que subtrai a quantidade de pontos
       }
     }
 
